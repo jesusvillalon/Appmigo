1e295069e10d52d6d8a7db4eb3fbb8da4d53329e
David Galisteo <dagalisteo@gmail.com>
2023-10-31 16:57:31 +0100

    add: blacklist route

--- Appmigo-Front/src/app/app-routing.module.ts
+++ Appmigo-Front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { GiftPageComponent } from './pages/gift-page/gift-page.component';
+import { BlacklistPageComponent } from './pages/blacklist-page/blacklist-page.component';
 
 const routes: Routes = [
-  {path: 'main-page', component: MainPageComponent},
+  {path: 'home', component: MainPageComponent},
   {path: 'gift-page', component: GiftPageComponent},
-  {path: '**', redirectTo: 'main-page'}
+  {path:'blacklist', component:BlacklistPageComponent},
+  {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
